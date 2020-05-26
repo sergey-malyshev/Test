883f864bd2d4ce664377e12c62778de70b0c836d
sergey <onega.malyshev@yandex.ru>
2020-05-26 19:50:18 +0300

    Add get methods for DataClass

--- myThread/src/myThread/DataClass.java
+++ myThread/src/myThread/DataClass.java
@@ -9,4 +9,12 @@ public class DataClass {
       this.key = key;
       this.value = value;
    }
+   
+   public String getKey() {
+      return key;
+   }
+   
+   public String getValue() {
+      return value;
+   }
 }
